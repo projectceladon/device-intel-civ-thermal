1d5f5b67db76a05ad485b2e79d92864bc5d469cb
saranya <saranya.gopal@intel.com>
2020-06-22 12:55:20 +0530

    thermal: Build host utility directly
    Previously, prebuilt binary was being copied for thermal
    host utility. Now that the code is merged into repo,
    build thermal host utility directly for target. Also,
    remove the prebuilt binary which will be unused now.
    
    Tracked-On: OAM-91484
    Signed-off-by: Saranya Gopal <saranya.gopal@intel.com>

--- vm_thermal_utility/Android.mk
+++ vm_thermal_utility/Android.mk
@@ -3,6 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := thermsys
 
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/scripts
+
 LOCAL_SRC_FILES := thermal_sysfsread.c
 
-include $(BUILD_EXECUTABLE)
+include $(BUILD_HOST_EXECUTABLE)
